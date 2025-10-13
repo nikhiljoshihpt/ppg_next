21a93913af97babf0dd14cc2980e78004a8e49c3
Rachit garg <rachitgarg56@gmail.com>
2025-10-13 11:56:22 +0530

    fixed UI in about us and contact page

--- src/app/(main)/about-us/page.tsx
+++ src/app/(main)/about-us/page.tsx
@@ -1,11 +1,12 @@
 import CoreValuesSection from '@/components/CoreValuesSection';
 import { Metadata } from 'next';
+import Link from 'next/link';
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const teamMembers = [
-    {name: "Kourtney Houston", designation: "Owner", blurb: "After a career as a cosmetologist, Kourtney was ready to go on the greatest adventure of her life when she founded PPG in 2011. A master of staffing and problem solving, she loves the exciting space of events and interactive marketing. Outside of work, Kourtney enjoys cooking for friends and family.", image: "/assets/images/about/kourtney-profile.jpg", },
-    {name: "Shale Houston", designation: "Owner", blurb: "Shale joined PPG during the pandemic, bringing 20+ years of sales and business experience and a willingness to take charge. He enjoys working in this industry precisely because there’s a different challenge every day. When not working, Shale enjoys hosting friends and family and boating.", image: "/assets/images/about/shale-profile.jpeg", },
+    {name: "Kourtney Houston", designation: "Owner", blurb: "After a career as a cosmetologist, Kourtney was ready to go on the greatest adventure of her life when she founded PPG in 2011. A master of staffing and problem solving, she loves the exciting space of events and interactive marketing. Outside of work, Kourtney enjoys cooking for friends and family.", image: "/assets/images/about/kourtney-profile.jpg", linkedIn: "https://www.linkedin.com/in/kourtney-houston-26768264/", },
+    {name: "Shale Houston", designation: "Owner", blurb: "Shale joined PPG during the pandemic, bringing 20+ years of sales and business experience and a willingness to take charge. He enjoys working in this industry precisely because there’s a different challenge every day. When not working, Shale enjoys hosting friends and family and boating.", image: "/assets/images/about/shale-profile.jpeg", linkedIn: "https://www.linkedin.com/in/shale-houston-9b186251/", },
     // {name: "Carrie Schoening", designation: "Manager", blurb: "Almost nothing happens here without Carrie’s effort and involvement. Carrie is the backend and calendar manager at PPG and has been a central member since 2014. Outside of work, Carrie is busy with her children and rescue animals, including the Irish wolfhounds she raises.", image: "", },
   ]
 
@@ -36,14 +37,19 @@ const page = () => {
                 {
                     teamMembers.map((member) => {
                         return (
-                            <div key={uuidv4()} className="grid grid-cols-1 md:grid-cols-2 md:gap-4 justify-items-center items-center">
-                                <figure className="w-64 md:w-80 h-64 md:h-80 bg-gray-100 rounded-full overflow-hidden">
+                            <div key={uuidv4()} className="flex flex-col md:flex-row md:gap-16 justify-items-center items-center">
+                                <figure className="w-64 md:w-80 h-64 md:h-80 bg-gray-100 rounded-full overflow-hidden shrink-0">
                                   <img className='w-full' src={member.image} alt={member.name} />
                                 </figure>
                                 <div className="leading-relaxed mt-8 md:mt-0">
                                     <h2 className="text-center md:text-left text-2xl md:text-3xl font-semibold italic font-cambria">{member.name}</h2>
                                     <h3 className="text-center md:text-left text-xl md:text-2xl font-thin italic mt-1">{member.designation}</h3>
                                     <p className="text-xl text-center md:text-left md:text-2xl mt-4 md:mt-8 font-thin">{member.blurb}</p>
+                                    <Link href={member.linkedIn} target="_blank" rel="noopener noreferrer">
+                                      <figure className='w-20 mt-4 max-md:mx-auto rounded overflow-hidden'>
+                                        <img src="/assets/images/linkedin-black-new.png" alt="linkedin-icon" />
+                                      </figure>
+                                    </Link>
                                 </div>
                             </div>
                         )
--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -18,15 +18,18 @@ export const generateMetadata = (): Metadata => {
         type: "website",
       },
     };
-  };
+};
 
 const page = () => {
     return (
         <div className='bg-gradient-to-r from-[#125F6C] via-[#1C7A8A] via-30% to-[#178DA1] text-white px-4 flex-1 flex flex-col gap-8 items-center max-sm:pt-16 sm:justify-center'>
             <div className="text-center text-5xl font-bold text-white italic font-cambria">Get in touch</div>
-            <div className="text-center text-3xl md:text-4xl font-bold italic text-gray-900 font-cambria">(123) 456 7890</div>
-            <div className="text-center font-cambria font-bold sm:mb-20">
-                <Link href={`mailto:hello@ActivatePPG.com`} className="text-center text-2xl sm:text-3xl md:text-4xl font-bold border-b border-gray-900 italic text-gray-900">hello@ActivatePPG.com</Link>
+            <div className="text-center text-3xl md:text-4xl font-bold italic text-gray-900 font-cambria">(636) 279-5829<span className='hidden sm:inline-block mx-2'>|</span><br className='block sm:hidden' />(314) 223-3545
+</div>
+            <div className="text-center font-cambria font-bold sm:mb-20 text-2xl sm:text-3xl md:text-4xl italic text-gray-900">
+                <Link href={`mailto:KHouston@activateppg.com`} className="text-center border-b border-gray-900">KHouston@activateppg.com</Link>
+                <span className='hidden sm:inline-block mx-2'> | </span><br className='block sm:hidden' />
+                <Link href={`mailto:Shouston@activateppg.com`} className="text-center border-b border-gray-900">Shouston@activateppg.com</Link>
             </div>
         </div>
     )
